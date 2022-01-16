78834fe5f2e1a3a054983d2570e7c19b34891414
PetarVrdoljak <73475134+PetarVrdoljak@users.noreply.github.com>
2022-01-16 15:22:05 +0100

    Add files via upload

---
+++ 2021_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('surname',50);
+            $table->date('date_of_birth');
+            $table->string('gender',10);
+            $table->foreignId('place_id')->constrained('places');
+            $table->foreignId('passport_id')->constrained('passports');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
